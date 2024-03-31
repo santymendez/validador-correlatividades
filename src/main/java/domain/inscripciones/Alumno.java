50211212a5b4622c5d39acffc4b89cea98972081
santymendez <santiagomendez532@gmail.com>
2024-03-31 20:32:59 -0300

    solucion mejorada

--- src/main/java/domain/inscripciones/Alumno.java
+++ src/main/java/domain/inscripciones/Alumno.java
@@ -7,22 +7,6 @@ public class Alumno {
     private String nombre;
     private List<Materia> materiasAp;
 
-    public String getLegajo() {
-        return legajo;
-    }
-
-    public void setLegajo(String legajo) {
-        this.legajo = legajo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public Alumno(String legajo, String nombre, List<Materia> materiasAp) {
         this.legajo = legajo;
         this.nombre = nombre;
